af9cbf100f0ef80ee57fbef75a56d0009b633289
Randy C. Will <randall.will@gmail.com>
2023-12-26 13:55:39 -0700

    Update to v5-pre42.  Closes #1.

--- outfox.sh
+++ outfox.sh
@@ -3,10 +3,11 @@ sudo apt dist-upgrade
 #sudo apt purge pulseaudio* pipewire*
 
 cd ~/Downloads/
-wget https://github.com/TeamRizu/OutFox/releases/download/OF4.19.0/OutFox-0.4.19-LTS-Linux-Rpi64bit-arm64v8-modern-date-20230415.tar.gz
-gzip -cd OutFox-0.4.19-LTS-Linux-Rpi64bit-arm64v8-modern-date-20230415.tar.gz | tar xvf -
+wget https://github.com/TeamRizu/OutFox/releases/download/OF5.0.0-042/OutFox-alpha-0.5.0-pre042-Raspberry-Pi-Linux-18.04-arm64-arm64v8-modern-date-20231225.tar.gz
+gzip -cd OutFox-alpha-0.5.0-pre042-Raspberry-Pi-Linux-18.04-arm64-arm64v8-modern-date-20231225.tar.gz | tar xvf -
 cd ~/Desktop/
-ln -s ../Downloads/OutFox-alpha-0.4.19-arm64v8-modern-date-20230415 OutFox
+[ -L OutFox ] && mv OutFox OutFox-`date +%Y%m%d%H%M%S`
+ln -s ../Downloads/OutFox-alpha-0.5.0-pre042-18.04-arm64-arm64v8-modern-date-20231225/ OutFox
 
 cat | sudo tee /etc/xdg/autostart/OutFox.desktop <<EOF
 #!/usr/bin/env xdg-open
